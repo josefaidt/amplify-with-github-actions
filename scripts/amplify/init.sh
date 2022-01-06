b28c4e9115f2cdeadf6a2bba807e4e4cb84652a9
josefaidt <josef.aidt@gmail.com>
2022-01-06 17:24:26 -0600

    fix init

--- scripts/amplify/init.sh
+++ scripts/amplify/init.sh
@@ -2,13 +2,6 @@
 set -e
 IFS='|'
 
-# REACTCONFIG="{\
-# \"SourceDir\":\"src\",\
-# \"DistributionDir\":\"build\",\
-# \"BuildCommand\":\"npm run-script build\",\
-# \"StartCommand\":\"npm run-script start\"\
-# }"
-
 AWSCLOUDFORMATIONCONFIG="{\
 \"configLevel\":\"project\",\
 \"useProfile\":false,\
@@ -19,21 +12,17 @@ AWSCLOUDFORMATIONCONFIG="{\
 }"
 
 AMPLIFY="{\
-\"envName\":\"$ENV_NAME\"
+\"envName\":\"$ENV_NAME\",\
+\"defaultEditor\":\"code\"\
 }"
-
-# FRONTEND="{\
-# \"frontend\":\"javascript\",\
-# \"framework\":\"react\",\
-# \"config\":$REACTCONFIG\
-# }"
 
 PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
 
+echo "Initializing Amplify project for env: $ENV_NAME"
+
 amplify init \
 --amplify $AMPLIFY \
-# --frontend $FRONTEND \
 --providers $PROVIDERS \
 --yes
